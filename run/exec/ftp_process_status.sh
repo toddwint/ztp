67b88aac9b6a2f39b2e1082212c88b35a80bc3b8
toddwint <todd.wintermute@gmail.com>
2022-08-31 16:21:46 -0500

    added ftp process start time information

--- run/exec/ftp_process_status.sh
+++ run/exec/ftp_process_status.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "$(dirname "$(dirname "$(realpath $0)")")"/config.txt
 
-docker exec "$HOSTNAME" ps ax | grep vsftpd:
+docker exec "$HOSTNAME" ps ax -eo pid,lstart,cmd | grep vsftpd:
