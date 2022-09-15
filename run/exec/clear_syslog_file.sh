a6d50b696530f554ffee52a578c8a61e81f1a32c
toddwint <todd.wintermute@gmail.com>
2022-09-14 21:45:50 -0500

    blank syslog file with truncate instead of echo

--- run/exec/clear_syslog_file.sh
+++ run/exec/clear_syslog_file.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "$(dirname "$(dirname "$(realpath $0)")")"/config.txt
 
-docker exec "$HOSTNAME" bash -c "echo '' > /var/log/syslog"
+docker exec "$HOSTNAME" truncate -s 0 /var/log/syslog
